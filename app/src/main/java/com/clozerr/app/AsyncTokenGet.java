86cd0e92c66be463b579e6b0981cb52995a07b52
Aravind Shankar <shankaravind167@gmail.com>
2015-01-23 12:21:05 +0530

    Oops... here is commit with AsyncTokenGet.java

---
+++ app/src/main/java/com/clozerr/app/AsyncTokenGet.java
@@ -0,0 +1,73 @@
+package com.clozerr.app;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.auth.UserRecoverableAuthException;
+import com.google.android.gms.plus.Plus;
+
+import java.io.IOException;
+
+/**
+ * Created by S.ARAVIND on 1/23/2015.
+ */
+public class AsyncTokenGet extends AsyncTask<Void, Void, String> {
+
+    Context mContext;
+    AsyncTokenResult mResult;
+    ProgressDialog pDialog;
+    public AsyncTokenGet(Context context, AsyncTokenResult result) {
+        mContext = context;
+        mResult = result;
+
+        pDialog = new ProgressDialog(context);
+        pDialog.setMessage("Loading...");
+        //pDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        pDialog.setCancelable(false);
+        pDialog.show();
+        this.execute();
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+        String token;
+        String scopes = "oauth2:" +
+                "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/plus.login";
+        try {
+            token = GoogleAuthUtil.getToken(
+                    mContext,
+                    Plus.AccountApi.getAccountName(Login.mGoogleApiClient),
+                    scopes);
+            // Log.e("AccessToken", token);
+        } catch (IOException transientEx) {
+            // network or server error, the call is expected to succeed if you try again later.
+            // Don't attempt to call again immediately - the request is likely to
+            // fail, you'll hit quotas or back-off.
+            return null;
+        } catch (UserRecoverableAuthException e) {
+            // Recover
+            token = null;
+        } catch (GoogleAuthException authEx) {
+            // Failure. The call is not expected to ever succeed so it should not be
+            // retried.
+            return null;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return token;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        pDialog.hide();
+        mResult.gotResult(result);
+    }
+
+    public static abstract class AsyncTokenResult{
+        public abstract void gotResult(String s);
+    }
+}
