77a30e6f735c02d437cf049b0e3d07206d3f625c
Aravind Shankar <shankaravind167@gmail.com>
2015-03-07 00:06:44 +0530

    Fixed location listener cup.

--- app/src/main/java/com/clozerr/app/MyLocation.java
+++ app/src/main/java/com/clozerr/app/MyLocation.java
@@ -5,6 +5,8 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -18,10 +20,12 @@ public class MyLocation {
     LocationResult locationResult;
     boolean gps_enabled = false;
     boolean network_enabled = false;
+    private Handler mHandler;
 
     public boolean getLocation(Context context, LocationResult result) {
         //I use LocationResult callback class to pass location value from MyLocation to user code.
         locationResult = result;
+        mHandler = new Handler(Looper.getMainLooper());
         if (lm == null)
             lm = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
 
@@ -65,37 +69,42 @@ public class MyLocation {
         @Override
         public void run() {
 
-            lm.removeUpdates(locationListenerGps);
-            lm.removeUpdates(locationListenerNetwork);
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    lm.removeUpdates(locationListenerGps);
+                    lm.removeUpdates(locationListenerNetwork);
 
-            Location net_loc = null, gps_loc = null;
-            if (gps_enabled)
-                gps_loc = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            if (network_enabled)
-                net_loc = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    Location net_loc = null, gps_loc = null;
+                    if (gps_enabled)
+                        gps_loc = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    if (network_enabled)
+                        net_loc = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
 
-            //if there are both values use the latest one
-            if (gps_loc != null && net_loc != null) {
-                if (gps_loc.getTime() > net_loc.getTime())
-                    locationResult.gotLocation(gps_loc);
-                else
-                    locationResult.gotLocation(net_loc);
-                return;
-            }
+                    //if there are both values use the latest one
+                    if (gps_loc != null && net_loc != null) {
+                        if (gps_loc.getTime() > net_loc.getTime())
+                            locationResult.gotLocation(gps_loc);
+                        else
+                            locationResult.gotLocation(net_loc);
+                        return;
+                    }
 
-            if (gps_loc != null) {
-               try{
-                   locationResult.gotLocation(gps_loc);
-               }catch (Exception e){
-                   e.printStackTrace();
-               }
-               return;
-            }
-            if (net_loc != null) {
-                locationResult.gotLocation(net_loc);
-                return;
-            }
-            locationResult.gotLocation(null);
+                    if (gps_loc != null) {
+                        try{
+                            locationResult.gotLocation(gps_loc);
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                        return;
+                    }
+                    if (net_loc != null) {
+                        locationResult.gotLocation(net_loc);
+                        return;
+                    }
+                    locationResult.gotLocation(null);
+                }
+            });
         }
     }
 
