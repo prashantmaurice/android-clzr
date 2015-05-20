d7562b3865d4426dacd399f347a1faf7323cc291
badarsh2 <badarsh2@gmail.com>
2015-05-20 14:02:35 +0530

    MyStamps layout and code interchange

--- app/src/main/java/com/clozerr/app/MyStampsFragment.java
+++ app/src/main/java/com/clozerr/app/MyStampsFragment.java
@@ -1,36 +1,286 @@
 package com.clozerr.app;
 
-/**
- * Created by Adarsh on 20-05-2015.
- */
-
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.TextView;
 
 
 public class MyStampsFragment extends Fragment {
 
     Context c;
     View layout;
-
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState) {
         layout = inflater.inflate(R.layout.activity_mystamps_fragment, container, false);
-        final ListView listview=(ListView)layout.findViewById(R.id.freebietypes);
-        final String[] values = new String[] { "Welcome Reward",
-                "Facebook Ckeck-In Reward",
-                "Happy Hour Reward",
-                "Lucky Reward"
-        };
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),R.layout.freebies_item_layout, R.id.freebiename, values);
-        listview.setAdapter(adapter);
+        final Resources reso = getResources();
+        final TextView num1 = (TextView)layout.findViewById(R.id.num1);
+        final TextView num2 = (TextView)layout.findViewById(R.id.num2);
+        final TextView num3 = (TextView)layout.findViewById(R.id.num3);
+        final TextView num4 = (TextView)layout.findViewById(R.id.num4);
+        final TextView num5 = (TextView)layout.findViewById(R.id.num5);
+        final TextView num6 = (TextView)layout.findViewById(R.id.num6);
+        final TextView num7 = (TextView)layout.findViewById(R.id.num7);
+        final TextView num8 = (TextView)layout.findViewById(R.id.num8);
+        final TextView num9 = (TextView)layout.findViewById(R.id.num9);
+        final TextView num10 = (TextView)layout.findViewById(R.id.num10);
+        num1.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View arg0, MotionEvent arg1)
+            {
+                switch(arg1.getActionMasked())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                    {
+                        TextView tv = (TextView)layout.findViewById(arg0.getId());
+                        tv.setBackground((GradientDrawable) reso.getDrawable(R.drawable.cirkbackhover));
+                        return true;
+
+                    }
+                    case MotionEvent.ACTION_UP:
+                        // TODO Auto-generated method stub
+                    {
+                        TextView tv = (TextView)layout.findViewById(arg0.getId());
+                        tv.setBackground((GradientDrawable)reso.getDrawable(R.drawable.circularback));
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+        num2.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View arg0, MotionEvent arg1)
+            {
+                switch(arg1.getActionMasked())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                    {
+                        TextView tv = (TextView)layout.findViewById(arg0.getId());
+                        tv.setBackground((GradientDrawable) reso.getDrawable(R.drawable.cirkbackhover));
+                        return true;
+
+                    }
+                    case MotionEvent.ACTION_UP:
+                        // TODO Auto-generated method stub
+                    {
+                        TextView tv = (TextView)layout.findViewById(arg0.getId());
+                        tv.setBackground((GradientDrawable)reso.getDrawable(R.drawable.circularback));
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+        num3.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View arg0, MotionEvent arg1)
+            {
+                switch(arg1.getActionMasked())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                    {
+                        TextView tv = (TextView)layout.findViewById(arg0.getId());
+                        tv.setBackground((GradientDrawable) reso.getDrawable(R.drawable.cirkbackhover));
+                        return true;
+
+                    }
+                    case MotionEvent.ACTION_UP:
+                        // TODO Auto-generated method stub
+                    {
+                        TextView tv = (TextView)layout.findViewById(arg0.getId());
+                        tv.setBackground((GradientDrawable)reso.getDrawable(R.drawable.circularback));
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+        num4.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View arg0, MotionEvent arg1)
+            {
+                switch(arg1.getActionMasked())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                    {
+                        TextView tv = (TextView)layout.findViewById(arg0.getId());
+                        tv.setBackground((GradientDrawable) reso.getDrawable(R.drawable.cirkbackhover));
+                        return true;
+
+                    }
+                    case MotionEvent.ACTION_UP:
+                        // TODO Auto-generated method stub
+                    {
+                        TextView tv = (TextView)layout.findViewById(arg0.getId());
+                        tv.setBackground((GradientDrawable)reso.getDrawable(R.drawable.circularback));
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+        num5.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View arg0, MotionEvent arg1)
+            {
+                switch(arg1.getActionMasked())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                    {
+                        TextView tv = (TextView)layout.findViewById(arg0.getId());
+                        tv.setBackground((GradientDrawable) reso.getDrawable(R.drawable.cirkbackhover));
+                        return true;
+
+                    }
+                    case MotionEvent.ACTION_UP:
+                        // TODO Auto-generated method stub
+                    {
+                        TextView tv = (TextView)layout.findViewById(arg0.getId());
+                        tv.setBackground((GradientDrawable)reso.getDrawable(R.drawable.circularback));
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+        num6.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View arg0, MotionEvent arg1)
+            {
+                switch(arg1.getActionMasked())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                    {
+                        TextView tv = (TextView)layout.findViewById(arg0.getId());
+                        tv.setBackground((GradientDrawable) reso.getDrawable(R.drawable.cirkbackhover));
+                        return true;
+
+                    }
+                    case MotionEvent.ACTION_UP:
+                        // TODO Auto-generated method stub
+                    {
+                        TextView tv = (TextView)layout.findViewById(arg0.getId());
+                        tv.setBackground((GradientDrawable)reso.getDrawable(R.drawable.circularback));
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+        num7.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View arg0, MotionEvent arg1)
+            {
+                switch(arg1.getActionMasked())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                    {
+                        TextView tv = (TextView)layout.findViewById(arg0.getId());
+                        tv.setBackground((GradientDrawable) reso.getDrawable(R.drawable.cirkbackhover));
+                        return true;
+
+                    }
+                    case MotionEvent.ACTION_UP:
+                        // TODO Auto-generated method stub
+                    {
+                        TextView tv = (TextView)layout.findViewById(arg0.getId());
+                        tv.setBackground((GradientDrawable)reso.getDrawable(R.drawable.circularback));
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+        num8.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View arg0, MotionEvent arg1)
+            {
+                switch(arg1.getActionMasked())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                    {
+                        TextView tv = (TextView)layout.findViewById(arg0.getId());
+                        tv.setBackground((GradientDrawable) reso.getDrawable(R.drawable.cirkbackhover));
+                        return true;
+
+                    }
+                    case MotionEvent.ACTION_UP:
+                        // TODO Auto-generated method stub
+                    {
+                        TextView tv = (TextView)layout.findViewById(arg0.getId());
+                        tv.setBackground((GradientDrawable)reso.getDrawable(R.drawable.circularback));
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+        num9.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View arg0, MotionEvent arg1)
+            {
+                switch(arg1.getActionMasked())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                    {
+                        TextView tv = (TextView)layout.findViewById(arg0.getId());
+                        tv.setBackground((GradientDrawable) reso.getDrawable(R.drawable.cirkbackhover));
+                        return true;
+
+                    }
+                    case MotionEvent.ACTION_UP:
+                        // TODO Auto-generated method stub
+                    {
+                        TextView tv = (TextView)layout.findViewById(arg0.getId());
+                        tv.setBackground((GradientDrawable)reso.getDrawable(R.drawable.circularback));
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+        num10.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View arg0, MotionEvent arg1)
+            {
+                switch(arg1.getActionMasked())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                    {
+                        TextView tv = (TextView)layout.findViewById(arg0.getId());
+                        tv.setBackground((GradientDrawable) reso.getDrawable(R.drawable.cirkbackhover));
+                        return true;
+
+                    }
+                    case MotionEvent.ACTION_UP:
+                        // TODO Auto-generated method stub
+                    {
+                        TextView tv = (TextView)layout.findViewById(arg0.getId());
+                        tv.setBackground((GradientDrawable)reso.getDrawable(R.drawable.circularback));
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
         return layout;
     }
     @Override
@@ -39,4 +289,3 @@ public class MyStampsFragment extends Fragment {
         c=activity;
     }
 }
-
