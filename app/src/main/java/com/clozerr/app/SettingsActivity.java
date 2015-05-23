78e0204ebfd56606cedf2bfe809ecedc5fd651cc
srivatsansoft <sriramesh4@gmail.com>
2015-05-23 10:56:19 +0530

    settings fragment

--- app/src/main/java/com/clozerr/app/SettingsActivity.java
+++ app/src/main/java/com/clozerr/app/SettingsActivity.java
@@ -2,25 +2,22 @@ package com.clozerr.app;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class SettingsActivity extends ActionBarActivity {
     private Toolbar toolbar;
-    private SettingsFragment mSettingsFragment;
+    private VendorSettingsFragment.SettingsFragment mSettingsFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         toolbar = (Toolbar) findViewById(R.id.settingtoolbar);
-        mSettingsFragment = new SettingsFragment();
+        mSettingsFragment = new VendorSettingsFragment.SettingsFragment();
         if (toolbar != null) {
             toolbar.setTitle("");
             setSupportActionBar(toolbar);
@@ -39,51 +36,5 @@ public class SettingsActivity extends ActionBarActivity {
         }
     }
 
-    public static class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
-        public SettingsFragment() {
-        }
 
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.preferences);
-
-        }
-
-        @Override
-        public void onDestroy() {
-            super.onDestroy();
-
-        }
-        @Override
-        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-            if (key.equals(getString(R.string.beacon_detection))) {
-                if (sharedPreferences.getBoolean(key, true))
-                    BeaconFinderService.allowScanning(getActivity());
-                else
-                    BeaconFinderService.disallowScanning(getActivity());
-            }
-        }
-        // TODO implement turning off notifications
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_settings, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-
-
-        return super.onOptionsItemSelected(item);
-    }
 }
