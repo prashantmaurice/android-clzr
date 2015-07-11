5c3b935cd655e64ef7c6a429ee8e0b1c01905cb5
raakeshkamal <raakeshkamal@gmail.com>
2015-07-11 05:53:49 +0530

    pinned over2

--- app/src/main/java/com/clozerr/app/FreebieDescription.java
+++ app/src/main/java/com/clozerr/app/FreebieDescription.java
@@ -2,6 +2,9 @@ package com.clozerr.app;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -15,6 +18,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.TextView;
@@ -22,16 +26,24 @@ import android.widget.Toast;
 
 import com.google.android.gcm.GCMRegistrar;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 
 public class FreebieDescription extends ActionBarActivity {
     String offerid="";
     String vendorid="",caption="",description="";
+    SharedPreferences status;
+    String NotNow;
+    ArrayList<String> pinned;
+    ImageButton pin;
+    ImageButton whatsappshare;
+    ImageButton share;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,7 +56,11 @@ public class FreebieDescription extends ActionBarActivity {
         });
         final FrameLayout freebielayout=(FrameLayout)findViewById(R.id.freebiesdesclayout);
         freebielayout.getForeground().setAlpha(0);
+        pin = (ImageButton) findViewById(R.id.pin);
+        whatsappshare = (ImageButton) findViewById(R.id.whatsappshare);
+        share = (ImageButton) findViewById(R.id.share);
         Intent intent = getIntent();
+        updatePind();
         try {
             offerid += intent.getStringExtra("offerid");
         }catch (Exception e){
@@ -72,6 +88,10 @@ public class FreebieDescription extends ActionBarActivity {
         if(!description.equals("")){
             ((TextView)findViewById(R.id.description)).setText(description);
         }
+        if(pinned.indexOf(offerid)==-1)
+           pin.setImageResource(R.drawable.pin100);
+        else
+           pin.setImageResource(R.drawable.pinfilled);
         findViewById(R.id.useit).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -141,8 +161,106 @@ public class FreebieDescription extends ActionBarActivity {
                 });
             }
         });
-    }
+        findViewById(R.id.pin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String urlPinning = "http://api.clozerr.com/v2/user/add/pinned?access_token=" + Home.TOKEN +"&offer_id="+offerid;
+                if(pinned.indexOf(offerid)==-1)
+                {
+                    new AsyncGet(getApplicationContext(), urlPinning, new AsyncGet.AsyncResult() {
+                        @Override
+                        public void gotResult(String s) {
+                            try {
+                                JSONObject obj = new JSONObject(s);
+                                final SharedPreferences.Editor editor = getSharedPreferences("USER", 0).edit();
+                                editor.putString("user",s);
+                                editor.apply();
+                                //Toast.makeText(getActivity(),s,Toast.LENGTH_SHORT).show();
+                                pin.setImageResource(R.drawable.pinfilled);
+                                Toast.makeText(getApplicationContext(), "Added to pinned offers", Toast.LENGTH_SHORT).show();
+                                //Toast.makeText(getActivity(),vendors.toString(),Toast.LENGTH_SHORT).show();
 
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                            //  t1.setText(s);
+                            if (s == null) {
+                                Toast.makeText(getApplicationContext(), "No internet connection", Toast.LENGTH_SHORT).show();
+                            }
+                            //l1.setAdapter(adapter);
+                        }
+                    });
+                }
+                else
+                {
+                    new AsyncGet(getApplicationContext(), "http://api.clozerr.com/v2/user/remove/pinned?access_token=" + Home.TOKEN +"&offer_id="+offerid, new AsyncGet.AsyncResult() {
+                        @Override
+                        public void gotResult(String s) {
+                            try {
+                                JSONObject obj = new JSONObject(s);
+                                final SharedPreferences.Editor editor = getSharedPreferences("USER", 0).edit();
+                                editor.putString("user",s);
+                                editor.apply();
+                                //Toast.makeText(getActivity(),s,Toast.LENGTH_SHORT).show();
+                                pin.setImageResource(R.drawable.pin100);
+                                Toast.makeText(getApplicationContext(), "Removed from pinned offers", Toast.LENGTH_SHORT).show();
+                                //Toast.makeText(getActivity(),vendors.toString(),Toast.LENGTH_SHORT).show();
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                            //  t1.setText(s);
+                            if (s == null) {
+                                Toast.makeText(getApplicationContext(), "No internet connection", Toast.LENGTH_SHORT).show();
+                            }
+                            //l1.setAdapter(adapter);
+                        }
+                    });
+                }
+            }
+        });
+        whatsappshare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickWhatsApp();
+            }
+        });
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                String shareBody = "Check out the offer I got @ " +VendorActivity.detailsBundle.getString("vendorTitle")+" using Clozerr: "+description+" https://play.google.com/store/apps/details?id=com.clozerr.app";
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Clozerr");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(sharingIntent, "Share via"));
+            }
+        });
+    }
+    public void updatePind(){
+        status = getSharedPreferences("USER", 0);
+        NotNow = status.getString("notNow","false");
+        pinned = new ArrayList<String>();
+        JSONArray pind ;
+        if(NotNow.equals("false")) {
+            try {
+                JSONObject userobj = new JSONObject(status.getString("user", "null"));
+                pind = userobj.getJSONArray("pinned");
+                Log.i("pinned", pind.toString());
+                if (pind != null) {
+                    int len = pind.length();
+                    for (int i = 0; i < len; i++) {
+                        pinned.add(pind.get(i).toString());
+                    }
+                }
+
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -190,5 +308,27 @@ public class FreebieDescription extends ActionBarActivity {
         });
 
         return popupWindow;
+    }
+    public void onClickWhatsApp() {
+
+        PackageManager pm=getPackageManager();
+        try {
+
+            Intent waIntent = new Intent(Intent.ACTION_SEND);
+            waIntent.setType("text/plain");
+            String text = "Check out the offer I got @ " +VendorActivity.detailsBundle.getString("vendorTitle")+" using Clozerr: "+description+" https://play.google.com/store/apps/details?id=com.clozerr.app";
+
+            PackageInfo info=pm.getPackageInfo("com.whatsapp", PackageManager.GET_META_DATA);
+            //Check if package exists or not. If not then code
+            //in catch block will be called
+            waIntent.setPackage("com.whatsapp");
+            waIntent.putExtra(Intent.EXTRA_TEXT, text);
+            startActivity(Intent.createChooser(waIntent, "Share with"));
+
+        } catch (PackageManager.NameNotFoundException e) {
+            Toast.makeText(getApplicationContext(), "WhatsApp not Installed", Toast.LENGTH_SHORT)
+                    .show();
+        }
+
     }
 }
